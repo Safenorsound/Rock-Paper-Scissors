1405e8e7c3e914e01701ed80d2635887e3cb86d7
Safenorsound <ijroback@gmail.com>
2020-05-13 14:58:05 -0700

    embed JS

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-styles.css
+console.log('Hello');
+
+const userScore = 0;
+const computerScore = 0;
+const userScorespam = document.getElementById('user-Score');
+const computerScorespan = document.getElementById('computer-Score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result');
+const rock_div = document.getElementById('.r');
+const paper_div = document.getElementById('.p');
+const scissors_div = document.getElementById('.s');
+rock_div.addEventListener('click', function () {
+  console.log('You clicked on rock');
+});
